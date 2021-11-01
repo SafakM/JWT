4e08d6f57dac7b580035750b308dcda1fa1f5f38
SafakM <68780545+SafakM@users.noreply.github.com>
2021-11-01 15:44:39 +0300

    first commit

---
+++ controllers/main.js
@@ -0,0 +1,34 @@
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  //   console.log(username, password);
+  //   res.send("Fake login/register/signup route");
+  res.status(200).json({ msg: "user created", token });
+};
+
+const dashboard = async (req, res) => {
+  console.log(req.headers);
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello safak`,
+    secret: `here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
